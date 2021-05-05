d1741457af1f024e4f634635fd698ff07b8ea624
ashNext <a.next.sh@gmail.com>
2021-05-05 19:51:48 +0300

    fix encoder

--- src/main/java/habr/telegram/bot/habrtelegrambot/HabrParseExample.java
+++ src/main/java/habr/telegram/bot/habrtelegrambot/HabrParseExample.java
@@ -1,18 +1,14 @@
 package habr.telegram.bot.habrtelegrambot;
 
-
 import habr.telegram.bot.habrtelegrambot.model.Post;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
-import java.io.IOException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class HabrParseExample {
 
@@ -21,19 +17,10 @@ public class HabrParseExample {
   private static final int MAX_POSTS = 75;
 
 
-  private final List<Post> posts = new LinkedList<>();
+  private final List<Post> posts = new ArrayList<>();
   private int pageCounter = 0;
 
   public static void main(String[] args) throws IOException {
-
-//    String tm = "23 Май 2021 сегодня";
-//    System.out.println(tm);
-//    System.out.println(System.getProperty("file.encoding"));
-//
-//    if (true) {
-//      return;
-//    }
-
     HabrParseExample parseHabr = new HabrParseExample();
     parseHabr.parsePage("/ru/");
     parseHabr.printPosts();
@@ -42,14 +29,15 @@ public class HabrParseExample {
   private void parsePage(String url) throws IOException {
     pageCounter++;
 
-    Document html = Jsoup.parse(new URL(SITE_URL + url).openStream(), "windows-1251", url);
+    Document html = Jsoup.connect(SITE_URL + url).get();
 
     Elements postsListElements = html.getElementsByClass("posts_list");
     if (postsListElements.size() == 0) {
       return;
     }
 
-    Elements contentList = postsListElements.get(0).getElementsByClass("content-list__item content-list__item_post shortcuts_item");
+    Elements contentList = postsListElements.get(0)
+        .getElementsByClass("content-list__item content-list__item_post shortcuts_item");
 
     List<Element> contentItems = contentList.stream()
         .filter(element -> element.attr("id").startsWith("post_"))
@@ -63,12 +51,10 @@ public class HabrParseExample {
 
       String time = contentItem.getElementsByClass("post__time").text();
 
-      // UTF_8
-      time = new String(time.getBytes(), StandardCharsets.UTF_8);
-
       Element postTitle = postTitles.get(0);
       if (postTitle.hasAttr("href") && postTitle.hasText()) {
-        posts.add(new Post(postTitle.attr("href"), postTitle.text(), DateTimeUtils.parseDateTime(time)));
+        posts.add(
+            new Post(postTitle.attr("href"), postTitle.text(), DateTimeUtils.parseDateTime(time)));
         if (posts.size() >= MAX_POSTS) {
           break;
         }
