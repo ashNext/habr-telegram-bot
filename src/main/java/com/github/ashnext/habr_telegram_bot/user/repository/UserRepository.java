5ebb93c3a5c61b554e7f6bf0d839b850dcddb3b2
a.shadrin <a.next.sh@gmail.com>
2023-04-26 17:16:29 +0300

    fix

--- src/main/java/com/github/ashnext/habr_telegram_bot/user/repository/UserRepository.java
+++ src/main/java/com/github/ashnext/habr_telegram_bot/user/repository/UserRepository.java
@@ -22,6 +22,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     @Query("SELECT DISTINCT u FROM User u LEFT JOIN FETCH u.hubs WHERE u.active=true AND u.subscription=true")
     List<User> findAllWithHubsByActiveAndSub();
 
-    @Query("SELECT u FROM User u JOIN FETCH u.hubs t WHERE u.id=:id AND t.hubGroup=:hubGroup")
+    @Query("SELECT u FROM User u JOIN FETCH u.hubs h WHERE u.id=:id AND h.hubGroup=:hubGroup")
     Optional<User> findWithHubsByIdAndHubGroup(@Param("id") UUID id, @Param("hubGroup") HubGroup hubGroup);
 }
