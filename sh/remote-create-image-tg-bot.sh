ffaee98df816c2abc5b21274594f7252c4a6738d
a.shadrin <a.next.sh@gmail.com>
2024-04-14 02:17:12 +0300

    fix

--- sh/remote-create-image-tg-bot.sh
+++ sh/remote-create-image-tg-bot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ssh root@185.154.195.193 /bin/bash << EOF
 rm -rf /root/habr-tg-bot
-git clone -b dev git@github.com:ashNext/habr-telegram-bot.git /root/habr-tg-bot
+git clone -b master git@github.com:ashNext/habr-telegram-bot.git /root/habr-tg-bot
 sh /root/habr-tg-bot/sh/tg-bot-create.sh
 EOF
